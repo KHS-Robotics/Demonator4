da5c7342644d55965d4b883c39d01d75f50a3277
Ernie-Wilson <ernestgwilsoniii@gmail.com>
2015-07-26 22:49:28 -0400

    Commented Setpoint class

--- src/org/usfirst/frc/team4342/robot/Setpoint.java
+++ src/org/usfirst/frc/team4342/robot/Setpoint.java
@@ -1,10 +1,23 @@
 package org.usfirst.frc.team4342.robot;
 
+/**
+ * 
+ * @author khsrobotics
+ * 
+ * This class is used for automatic elevator movement. The user can press
+ * a button on the joystick in order to move to a certain height of the
+ * elevator. This class is used for the SetpointMapWrapper
+ */
 public class Setpoint {
 	
 	private final int button;
 	private final int encoderCounts;
 	
+	/**
+	 * Constructs an elevator setpoint
+	 * @param button the button associated with the setpoint
+	 * @param encoderCounts the encoder counts for the setpoint
+	 */
 	public Setpoint(int button, int encoderCounts) {
 		this.button = button;
 		this.encoderCounts = encoderCounts;
