1508a50af24ca7f79e04b8f904ae22a4514c05b5
Magnus-Murray <ernestgwilsoniii@gmail.com>
2015-08-22 23:10:23 -0400

    added RobotConsoleLog.java to GitHub

---
+++ src/org/usfirst/frc/team4342/robot/logging/RobotConsoleLog.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team4342.robot.logging;
+
+import Logging.BaseLog;
+import Logging.Severity;
+import edu.wpi.first.wpilibj.DriverStation;
+
+/**
+ * @author Ernie
+ * 
+ * Logger that logs to the console on the driver station
+ */
+public class RobotConsoleLog extends BaseLog {
+
+	/**
+	 * Logs to the console on the driver station. This method only
+	 * logs the severity level and the message.
+	 */
+	@Override
+	public void log(Severity severity, Object... message) {
+		String mssg = severity.toString().toUpperCase() + ": ";
+
+		String hackMessage = message[2].toString();
+		hackMessage = hackMessage.substring(hackMessage.indexOf(":")+1);
+		
+		mssg += hackMessage;
+		
+		DriverStation.reportError(mssg, false);
+	}
+
+}
